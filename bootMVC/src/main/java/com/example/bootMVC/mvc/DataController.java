dadd47575d71ce1aa919dd33afe9fba3cc5a0835
chkvj <chkvj@windows10.microdone.cn>
2020-06-14 23:55:09 +0800

    Init Commit

---
+++ bootMVC/src/main/java/com/example/bootMVC/mvc/DataController.java
@@ -0,0 +1,20 @@
+package com.example.bootMVC.mvc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataController {
+
+
+	@GetMapping("/data")
+	public String get(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Data", name);
+	}
+	
+	@GetMapping("/dataList")
+	public String list(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Data List", name);
+	}
+}
